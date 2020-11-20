48c270d263484ce001134d89206d14e46bc27076
BaronTech <bayenguer97@gmail.com>
2020-11-20 10:07:09 +0000

    cors config

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const FilmController = require('../controller/FilmController');
+const cors = require('cors');
 
-router.get('/films', FilmController.findAll)
-router.post('/films', FilmController.createFilm)
-router.get('/films/:id', FilmController.showFilm)
-router.put('/films/:id', FilmController.updateFilm)
-router.delete('/films/:id', FilmController.deleteFilm)
+const corsOptions = {
+    origin: '*',
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 200
+}
+
+router.get('/films', cors(corsOptions), FilmController.findAll);
+router.post('/films', cors(corsOptions), FilmController.createFilm);
+router.get('/films/:id', cors(corsOptions), FilmController.showFilm);
+router.put('/films/:id', cors(corsOptions), FilmController.updateFilm);
+router.delete('/films/:id', cors(corsOptions), FilmController.deleteFilm);
 
 
 module.exports = router;
